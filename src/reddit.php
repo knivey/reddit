d28c98d6db9d48f9dec03689b974058b5341d6e2
knivey <knivey@botops.net>
2024-11-20 18:00:39 -0500

    Update for the latest amphp that uses fibers now

--- src/reddit.php
+++ src/reddit.php
@@ -5,7 +5,8 @@ use Amp\Http\Client\HttpClientBuilder;
 use Amp\Http\Client\HttpClient;
 use Amp\Http\Client\Interceptor\SetRequestHeaderIfUnset;
 use Amp\Http\Client\Request;
-use Amp\Promise;
+use Amp\Http\Client\Form;
+use Amp\Future;
 use Amp;
 
 
@@ -15,25 +16,32 @@ class reddit {
     protected string $device_id;
     protected HttpClient $httpClient;
 
+    //TODO add option for setting request timeouts
+
     function __construct(protected string $client_id, protected string $user_agent)
     {
         $this->device_id = bin2hex(random_bytes(21));
         $this->httpClient = (new HttpClientBuilder())->skipDefaultUserAgent()->intercept(new SetRequestHeaderIfUnset('user-agent', $user_agent))->build();
     }
 
-    public function renewToken(bool $force = false): Promise {
-        return \Amp\call (function () use ($force) {
+    /**
+     * 
+     * @param bool $force 
+     * @return Future<void> 
+     */
+    public function renewToken(bool $force = false): Future {
+        return \Amp\async (function () use ($force): void {
             if($this->tokenExpires -100 > time() && !$force)
                 return;
             $time = time();
             $r = new Request("https://www.reddit.com/api/v1/access_token", "POST");
-            $b = new Amp\Http\Client\Body\FormBody();
+            $b = new Form();
             $b->addField("grant_type", "https://oauth.reddit.com/grants/installed_client");
             $b->addField("device_id", $this->device_id);
             $r->setBody($b);
             $r->addHeader("Authorization", 'Basic ' . base64_encode($this->client_id . ':'));
-            $response = yield $this->httpClient->request($r);
-            $body = yield $response->getBody()->buffer();
+            $response = $this->httpClient->request($r);
+            $body = $response->getBody()->buffer();
             if ($response->getStatus() != 200) {
                 throw new \Exception("Reddit response status {$response->getStatus()} when renewing token");
             }
@@ -46,18 +54,23 @@ class reddit {
         });
     }
 
-    public function getCall($endpoint): Promise {
-        return Amp\call(function () use ($endpoint) {
-            yield $this->renewToken();
+    /**
+     * 
+     * @param string $endpoint 
+     * @return Future<mixed> json_decode of the response
+     */
+    public function getCall(string $endpoint): Future {
+        return Amp\async(function () use ($endpoint) {
+            $this->renewToken()->await();
             $retry = 0;
             while(true) {
                 if($retry > 0) {
-                    yield $this->renewToken(true);
+                    $this->renewToken(true)->await();
                 }
                 $r = new Request("https://oauth.reddit.com/$endpoint");
                 $r->addHeader("Authorization", "bearer {$this->access_token}");
-                $response = yield $this->httpClient->request($r);
-                $body = yield $response->getBody()->buffer();
+                $response = $this->httpClient->request($r);
+                $body = $response->getBody()->buffer();
                 if ($response->getStatus() != 200) {
                     if ($retry < 2) {
                         $retry++;
@@ -70,9 +83,14 @@ class reddit {
         });
     }
 
-    public function info($url): Promise {
-        return Amp\call(function () use ($url) {
-            return yield $this->getCall("api/info?url=".urlencode($url));
+    /**
+     * 
+     * @param string $url 
+     * @return Future<mixed> 
+     */
+    public function info(string $url): Future {
+        return Amp\async(function () use ($url) {
+            return $this->getCall("api/info?url=".urlencode($url));
         });
     }
 }
